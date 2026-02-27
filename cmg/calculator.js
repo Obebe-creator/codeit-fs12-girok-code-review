8c939aceb441cc902ab61908662f26780fc6a334
hogu-giriboy <choo6078@kakao.com>
2026-02-28 00:13:17 +0900

    ✨feat: [실습] 계산기 만들기 완료

---
+++ cmg/calculator.js
@@ -0,0 +1,38 @@
+// calculator.js
+
+// TODO: 두 숫자 변수 선언
+// 힌트: const num1 = ...
+
+const num1 = 10;
+const num2 = 5;
+
+// TODO: 덧셈 결과 계산 및 출력
+// 힌트: const sum = num1 + num2
+
+const sum = num1 + num2;
+
+console.log(`${num1} + ${num2} = ${sum}`);
+
+// TODO: 뺄셈 결과 계산 및 출력
+
+const sub = num1 - num2;
+
+console.log(`${num1} - ${num2} = ${sub}`);
+
+// TODO: 곱셈 결과 계산 및 출력
+
+const mul = num1 * num2;
+
+console.log(`${num1} * ${num2} = ${mul}`);
+
+// TODO: 나눗셈 결과 계산 및 출력
+
+const div = num1 / num2;
+
+console.log(`${num1} / ${num2} = ${div}`);
+
+// TODO: 나머지 계산 및 출력
+
+const rem = num1 % num2;
+
+console.log(`${num1} % ${num2} = ${rem}`);
