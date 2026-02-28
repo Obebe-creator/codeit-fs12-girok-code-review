ffd23dcebec4b9985d75fb894f9920c04547e855
Obebe <shkim000905@naver.com>
2026-02-28 09:20:02 +0900

    ✨feat: 계산기 기본 문제, 성적 기본 문제 작성

---
+++ ksh/calculator.js
@@ -0,0 +1,33 @@
+// TODO: 두 숫자 변수 선언
+// 힌트: const num1 = ...
+
+const num1 = 6;
+const num2 = 8;
+
+// TODO: 덧셈 결과 계산 및 출력
+// 힌트: const sum = num1 + num2
+const sum = num1 + num2;
+console.log(sum);
+
+// TODO: 뺄셈 결과 계산 및 출력
+const subtraction = num1 - num2;
+console.log(subtraction);
+
+// TODO: 곱셈 결과 계산 및 출력
+const multiplication = num1 - num2;
+console.log(multiplication);
+
+// TODO: 나눗셈 결과 계산 및 출력
+const division = num1 / num2;
+console.log(division);
+
+// TODO: 나머지 계산 및 출력
+const remain = num1 % num2;
+console.log(remain);
+
+
+// **도전 과제:**
+
+// 1. 복합 계산 추가 (예: (a + b) * c)
+// 2. 템플릿 리터럴로 출력 형식 개선
+// 3. 다른 숫자로도 테스트
